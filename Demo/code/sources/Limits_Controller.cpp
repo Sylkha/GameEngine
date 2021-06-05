365abf5ac7ae4f4ba994a1f55c14255999b58db5
Silvia Osoro <silwia.o.g@gmail.com>
2021-06-05 15:20:49 +0200

    1.0.1

--- Demo/code/sources/Limits_Controller.cpp
+++ Demo/code/sources/Limits_Controller.cpp
@@ -12,7 +12,7 @@ void Limits_Controller::update(Entity& entity, float deltatime) {
 	Vector3 playerPosition = extract_translation(player_Entity->get_transform());
 	Vector3 myPosition = extract_translation(entity.get_transform());
 	float speed = player_Controller.getSpeed();
-	/** Si está queriendo sobrepasar el límite, le desplazamos el equivalente a su velocidad. */
+	/** Si el jugador está queriendo sobrepasar el límite, le desplazamos el equivalente a su velocidad. */
 	switch (pos) {
 		case Up:
 			if (playerPosition.y > myPosition.y - 2) {
